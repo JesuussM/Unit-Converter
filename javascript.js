86ec174bd0a4ccfe915af757b6715fbb85bbcdc9
Jesus <jesusmirandaa55@gmail.com>
2023-11-08 00:02:48 -0700

    Semi-optimized unoptimized code

--- javascript.js
+++ javascript.js
@@ -14,77 +14,36 @@ function calculate() {
     let unit1 = document.getElementById("fromValue").value;
     let unit2 = document.getElementById("toValue").value;
 
-    // Perform the conversion if the first unit is mph
-    if (unit1 == "mph") {
-        // Convert from mph to various units
-         if (unit2 == "kph") {
-            number *= 1.60934;
-        } else if (unit2 == "fps") {
-            number *= 1.46667;
-        } else if (unit2 == "mps") {
-            number *= 0.44704;
-        } else if (unit2 == "knot") {
-            number *= 0.868976;
-        }
-    }
+    // Define the units and conversion factors
+    const units = new Map([
+        ["mph_to_mph", number],
+        ["mph_to_kph", number * 1.60934],
+        ["mph_to_fps", number * 1.46667],
+        ["mph_to_mps", number * 0.44704],
+        ["mph_to_knot", number * 0.868976],
+        ["kph_to_kph", number],
+        ["kph_to_mph", number * 0.621371],
+        ["kph_to_fps", number * 0.911344],
+        ["kph_to_mps", number * 0.277778],
+        ["kph_to_knot", number * 0.539957],
+        ["fps_to_fps", number],
+        ["fps_to_mph", number * 0.681818],
+        ["fps_to_kph", number * 1.09728],
+        ["fps_to_mps", number * 0.3048],
+        ["fps_to_knot", number * 0.592484],
+        ["mps_to_mps", number],
+        ["mps_to_mph", number * 2.23694],
+        ["mps_to_kph", number * 3.6],
+        ["mps_to_fps", number * 3.28084],
+        ["mps_to_knot", number * 1.94384],
+        ["knot_to_knot", number],
+        ["knot_to_mph", number * 1.15078],
+        ["knot_to_kph", number * 1.852],
+        ["knot_to_fps", number * 1.68781],
+        ["knot_to_mps", number * 0.514444]
+    ]);
     
-    // Perform the conversion if the first unit is kph
-    if (unit1 == "kph") {
-        // Convert from kph to various units
-        if (unit2 == "mph") {
-            number *= 0.621371;
-        } else if (unit2 == "fps") {
-            number *= 0.911344;
-        } else if (unit2 == "mps") {
-            number *= 0.277778;
-        } else if (unit2 == "knot") {
-            number *= 0.539957;
-        }
-    }
-
-    // Perform the conversion if the first unit is fps
-    if (unit1 == "fps") {
-        // Convert from fps to various units
-        if (unit2 == "mph") {
-            number *= 0.681818;
-        } else if (unit2 == "kph") {
-            number *= 1.09728;
-        } else if (unit2 == "mps") {
-            number *= 0.3048;
-        } else if (unit2 == "knot") {
-            number *= 0.592484;
-        }
-    }
-
-    // Perform the conversion if the first unit is mps
-    if (unit1 == "mps") {
-        // Convert from mps to various units
-        if (unit2 == "mph") {
-            number *= 2.23694;
-        } else if (unit2 == "kph") {
-            number *= 3.6;
-        } else if (unit2 == "fps") {
-            number *= 3.28084;
-        } else if (unit2 == "knot") {
-            number *= 1.94384;
-        }
-    }
-
-    // Perform the conversion if the first unit is knot
-    if (unit1 == "knot") {
-        // Convert from knot to various units
-        if (unit2 == "mph") {
-            number *= 1.15078;
-        } else if (unit2 == "kph") {
-            number *= 1.852;
-        } else if (unit2 == "fps") {
-            number *= 1.68781;
-        } else if (unit2 == "mps") {
-            number *= 0.514444;
-        } 
-    }
-
     // Display the result and unit
-    document.getElementById("output").innerHTML = number;
+    document.getElementById("output").innerHTML = units.get(unit1 + "_to_" + unit2);
     document.getElementById("result").innerHTML = unit2.charAt(0).toUpperCase() + unit2.slice(1);
 }
