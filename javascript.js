4bd7644cf300c1d0a32d41911f30d2a8e597021b
Jesus <jesusmirandaa55@gmail.com>
2023-11-09 00:12:52 -0700

    Optimized unoptimized code

--- javascript.js
+++ javascript.js
@@ -7,43 +7,35 @@
  * The calculate function retrieves the number and units from the HTML form,
  * performs the appropriate conversion based on the units selected, and
  * updates the HTML with the conversion result.
- */
+*/
+
+
 function calculate() {
+    
+    // Create a map of conversion factors
+    const units = new Map([
+        ["mph_to_mph", 1],
+        ["mph_to_kph", 1.60934],
+        ["mph_to_fps", 1.46667],
+        ["mph_to_mps", 0.44704],
+        ["mph_to_knot", 0.868976],
+    ]);
+
     // Retrieve the number and units from the HTML form
     let number = Number(document.getElementById("input").value);
     let unit1 = document.getElementById("fromValue").value;
     let unit2 = document.getElementById("toValue").value;
-
-    // Define the units and conversion factors
-    const units = new Map([
-        ["mph_to_mph", number],
-        ["mph_to_kph", number * 1.60934],
-        ["mph_to_fps", number * 1.46667],
-        ["mph_to_mps", number * 0.44704],
-        ["mph_to_knot", number * 0.868976],
-        ["kph_to_kph", number],
-        ["kph_to_mph", number * 0.621371],
-        ["kph_to_fps", number * 0.911344],
-        ["kph_to_mps", number * 0.277778],
-        ["kph_to_knot", number * 0.539957],
-        ["fps_to_fps", number],
-        ["fps_to_mph", number * 0.681818],
-        ["fps_to_kph", number * 1.09728],
-        ["fps_to_mps", number * 0.3048],
-        ["fps_to_knot", number * 0.592484],
-        ["mps_to_mps", number],
-        ["mps_to_mph", number * 2.23694],
-        ["mps_to_kph", number * 3.6],
-        ["mps_to_fps", number * 3.28084],
-        ["mps_to_knot", number * 1.94384],
-        ["knot_to_knot", number],
-        ["knot_to_mph", number * 1.15078],
-        ["knot_to_kph", number * 1.852],
-        ["knot_to_fps", number * 1.68781],
-        ["knot_to_mps", number * 0.514444]
-    ]);
+    
+    // converts to mph
+    let conversion = 1 / units.get("mph_to_" + unit1);
+    number *= conversion;
+    
+    // converts to unit2
+    conversion = units.get("mph_to_" + unit2);
+    number *= conversion;
     
     // Display the result and unit
-    document.getElementById("output").innerHTML = units.get(unit1 + "_to_" + unit2);
+    document.getElementById("output").innerHTML = number;
     document.getElementById("result").innerHTML = unit2.charAt(0).toUpperCase() + unit2.slice(1);
 }
+
